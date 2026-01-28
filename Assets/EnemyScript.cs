88d04fa24f47d316c879afc317a1f7649f7d14ef
amirmashhori81-creator <yasamanbagheri42@gmail.com>
2026-01-29 02:39:29 +0330

    p4

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public enum EnemyType
+{
+    Reproducible,
+    Friend,
+    Enemy,
+    Boss
+}
+
+public class EnemyScript : MonoBehaviour
+{
+    [Header("Enemy Settings")]
+    public EnemyType enemyType;
+
+    public float moveSpeed = 3f;
+
+    [Header("Direction Slice Settings")]
+    public float directionMin = 0f;
+    public float directionMax = 22.5f;
+
+    [Header("References")]
+
+    public game_maneger gameManager;
+
+    void Update()
+    {
+
+
+        MoveTowardsTarget();
+    }
+
+    void MoveTowardsTarget()
+    {
+        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime, Space.World);
+
+    }
+
+    public bool IsInSlice(float angle)
+    {
+        return angle >= directionMin && angle <= directionMax;
+    }
+
+
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("Door"))
+        {
+            if(enemyType == EnemyType.Friend)
+            {
+
+            }
+            if (gameManager != null)
+                gameManager.LostOneLife();
+
+            Destroy(gameObject);
+        }
+    }
+}
