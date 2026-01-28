a0dff7be93d6e70718c71fa087369665cfdba3a1
amirmashhori81-creator <yasamanbagheri42@gmail.com>
2026-01-29 02:40:53 +0330

    p5

---
+++ Assets/BossScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+
+[System.Serializable]
+public class angel_Holder
+{
+    public float min_angel = 0f;
+    public float max_angel = 0f;
+    public Vector3 rotation_image;
+
+};
+public class BossScript : MonoBehaviour
+{
+
+
+    [Header("Enemy Settings")]
+    public EnemyType enemyType = EnemyType.Boss;
+
+    public GameObject angel_imge;
+    public List<angel_Holder> angel_Holder;
+    public int index = 0;
+
+
+    void MoveTowardsTarget()
+    {
+   
+        transform.Translate(Vector2.right * 2f * Time.deltaTime, Space.World);
+    }
+
+
+    private void Start()
+    {
+        angel_imge.transform.Rotate(angel_Holder[0].rotation_image);
+    }
+
+
+
+    private void Update()
+    {
+        MoveTowardsTarget();
+    }
+    public bool IsInSlice(float angle)
+    {
+        return angle >= angel_Holder[index].min_angel && angle <= angel_Holder[index].max_angel;
+    }
+
+
+    public void add_index()
+    {
+        index++;
+     
+    }
+
+    public void rotate_angel_image()
+    {
+        // حذف تمامی رویت‌ها قبل از رویت جدید
+        angel_imge.transform.rotation = Quaternion.identity;
+        angel_imge.transform.Rotate(angel_Holder[index].rotation_image);
+    }
+
+}
---
+++ Assets/Enemy_Maker.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Maker : MonoBehaviour
+{
+    public List<GameObject> EnemyPrefabs = new List<GameObject>();
+
+    public float spawnInterval = 8f;
+
+    // فقط یک نقطه اسپاون
+    public Transform spawnPoint;
+
+    public int maxEnemiesToSpawn = 0; // 0 = بی‌نهایت
+    private int enemiesSpawned = 0;
+
+
+    public game_maneger game_Maneger;
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        while (maxEnemiesToSpawn == 0 || enemiesSpawned < maxEnemiesToSpawn)
+        {
+            // انتخاب یک دشمن تصادفی
+            int randomEnemyIndex = Random.Range(0, EnemyPrefabs.Count);
+            GameObject enemyPrefab = EnemyPrefabs[randomEnemyIndex];
+
+            // اسپاون دشمن در یک نقطه ثابت
+            Vector2 spawnPosition = spawnPoint.position;
+
+            GameObject enemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+
+            // ست کردن Target و GameManager
+            EnemyScript enemyScript = enemy.GetComponent<EnemyScript>();
+            if (enemyScript != null)
+            {
+                enemyScript.gameManager = game_Maneger;
+
+
+                enemiesSpawned++;
+            }
+           
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+}
